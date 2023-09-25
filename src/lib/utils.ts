496686f92d8b87a9aa05274d5b643b3fc4dc9af4
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-25 23:47:27 +0530

    chore: improve documentation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,6 +18,13 @@ const ensureStartsWith = (stringToCheck: string, startsWith: string) =>
     ? stringToCheck
     : `${startsWith}${stringToCheck}`;
 
+/**
+* Compare the prices of two products.
+*
+* @param {Product & { category: Category | null; providers: Provider[] }} a - The first product to compare.
+* @param {Product & { category: Category | null; providers: Provider[] }} b - The second product to compare.
+* @returns {number} - A negative number if `a` is cheaper, a positive number if `b` is cheaper, or 0 if they have the same price.
+*/
 const comparePrices = (a: (Product & { category: Category | null; providers: Provider[] }), b: (Product & { category: Category | null; providers: Provider[] })) => {
   const priceA = a.providers
     .map(({ price }) => +price.replace(/,/g, ""))
