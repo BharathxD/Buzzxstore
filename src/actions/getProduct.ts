01fc7140a44ffb5b57fe7cf50734f45fa1db8f12
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-25 23:49:42 +0530

    add: JSDoc-style documentation to getProduct function

--- src/actions/getProduct.ts
+++ src/actions/getProduct.ts
@@ -1,3 +1,10 @@
+/**
+ * Retrieves a product based on the provided product ID.
+ *
+ * @param {Object} params - Parameters for retrieving the product.
+ * @param {string} params.productId - The ID of the product to retrieve.
+ * @returns {Promise<ModifiedProducts | null>} - A promise that resolves to the modified product or null if an error occurs.
+ */
 "use server";
 
 import type { Product, Provider } from "@prisma/client";
@@ -6,6 +13,13 @@ import database from "@/lib/database";
 
 export type ModifiedProducts = { providers: Provider[] } & Product;
 
+/**
+ * Retrieves a product based on the provided product ID.
+ *
+ * @param {Object} params - Parameters for retrieving the product.
+ * @param {string} params.productId - The ID of the product to retrieve.
+ * @returns {Promise<ModifiedProducts | null>} - A promise that resolves to the modified product or null if an error occurs.
+ */
 async function getProduct({
   productId,
 }: {
