65a5eec62e604311da0e30518f26cbb1284895b1
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-25 23:50:36 +0530

    add JSDoc-style documentation to getCategories function

--- src/actions/getCategories.ts
+++ src/actions/getCategories.ts
@@ -1,9 +1,19 @@
+/**
+ * Retrieves a list of categories.
+ *
+ * @returns {Promise<Category[] | null>} - A promise that resolves to an array of categories or null if an error occurs.
+ */
 "use server";
 
 import { type Category } from "@prisma/client";
 
 import database from "@/lib/database";
 
+/**
+ * Retrieves a list of categories.
+ *
+ * @returns {Promise<Category[] | null>} - A promise that resolves to an array of categories or null if an error occurs.
+ */
 async function getCategories(): Promise<Category[] | null> {
   try {
     const categories = await database.category.findMany();
