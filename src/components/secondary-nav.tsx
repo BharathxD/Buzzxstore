c4e61592bb030d686bf90346ede8642c40e0d720
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-28 16:07:07 +0530

    chore: improve variable naming

--- src/components/secondary-nav.tsx
+++ src/components/secondary-nav.tsx
@@ -6,12 +6,13 @@ import NavItem from "./nav-item";
 import OtherCategories from "./other-categories";
 import HomeButton from "./ui/home-button";
 
+const SPLIT_INDEX = 4;
+
 const SecondaryNav = async () => {
   const categories = await getCategories();
   if (!categories) return null;
-  const splitIndex = 4;
-  const firstCategorySplit = categories.slice(0, splitIndex);
-  const secondCategorySplit = categories.slice(splitIndex);
+  const firstCategorySplit = categories.slice(0, SPLIT_INDEX);
+  const secondCategorySplit = categories.slice(SPLIT_INDEX);
   return (
     <div className="no-scrollbar flex h-[7vh] w-full flex-row overflow-x-scroll border-b border-b-neutral-700 bg-neutral-900/90 p-2 backdrop-blur-md">
       <div className="flex w-full flex-row items-center gap-4 px-4 md:justify-center">
