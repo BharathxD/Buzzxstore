ea09a5e606e731375620a1b60d4e2abfb9563822
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-10-06 22:34:56 +0530

    feat: add discount and actual price on product page

--- src/components/product-description.tsx
+++ src/components/product-description.tsx
@@ -25,14 +25,19 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
     MAX_DESCRIPTION_LENGTH
   );
 
-  const renderProviderLink = (provider: Provider, index: number) => {
-    const { id, name, link } = provider;
+  const ProviderLink = ({
+    name,
+    link,
+  }: {
+    id: string;
+    name: string;
+    link: string;
+  }) => {
     const isAmazon = name === "Amazon";
     const IconComponent = isAmazon ? FaAmazon : SiFlipkart;
 
     return (
       <Link
-        key={`${id}_${index}`}
         className={cn(
           "inline-flex items-center justify-center gap-2 rounded-md bg-secondary px-4 py-2 text-sm font-medium text-secondary-foreground ring-offset-background transition-colors hover:bg-secondary/80 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
           {
@@ -55,20 +60,26 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
         <h1 className="bg-gradient-to-br from-zinc-200 to-zinc-400 bg-clip-text text-[25px] font-extrabold leading-tight tracking-tighter text-transparent md:text-[48px]">
           {displayText}
         </h1>
-        <div className="mr-auto flex w-full items-center justify-between gap-5 rounded-full text-white md:flex-row">
-          <span className="text-2xl md:text-3xl">
-            <Price
-              amount={Math.max(
-                ...product.providers.map(({ price }) =>
-                  parseFloat(price.replace(/,/g, ""))
-                )
-              )}
-            />
-          </span>
-          <span className="inline-flex gap-2">
-            {product.providers.map(renderProviderLink)}
-          </span>
-        </div>
+        {product.providers.map(({ id, price, link, name, mrp, off }) => {
+          return (
+            <div
+              className="mr-auto flex w-full items-center justify-between gap-5 rounded-full text-white md:flex-row"
+              id={id}
+            >
+              <span className="p-1 text-2xl md:text-3xl">
+                <Price
+                  amount={parseFloat(price.replace(/,/g, ""))}
+                  mrp={mrp}
+                  off={off}
+                  isProductPage
+                />
+              </span>
+              <span className="inline-flex gap-2">
+                <ProviderLink id={id} link={link} name={name} />
+              </span>
+            </div>
+          );
+        })}
       </div>
       <div className="prose prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-a:underline hover:prose-a:text-neutral-300 prose-ol:mt-8 prose-ol:list-decimal prose-ol:pl-6 prose-ul:mt-8 prose-ul:list-disc prose-ul:pl-6 prose-a:text-white prose-strong:text-white mx-auto mb-6 max-w-6xl text-base leading-snug text-neutral-300">
         {displayDescription}
--- src/components/ui/price.tsx
+++ src/components/ui/price.tsx
@@ -1,16 +1,43 @@
 import clsx from "clsx";
 
+import { cn } from "@/lib/utils";
+
 const Price = ({
   amount,
+  mrp,
+  off,
   className,
+  isProductPage = false,
 }: {
   amount: number;
+  mrp?: string;
+  off?: string;
   className?: string;
-} & React.ComponentProps<"p">) => (
-  <p suppressHydrationWarning={true} className={className}>
-    <span className={clsx("ml-1 inline")}>&#x20B9;</span>
-    <span>{amount}</span>
-  </p>
-);
+  isProductPage?: boolean;
+} & React.ComponentProps<"div">) =>
+  isProductPage ? (
+    <div
+      suppressHydrationWarning={true}
+      className={cn("flex flex-col gap-2", className)}
+    >
+      <div className="flex flex-row gap-2">
+        <span className={clsx("inline")}>
+          &#x20B9;
+          {amount}
+        </span>
+        <span className="h-fit w-fit rounded-md border border-zinc-600 bg-zinc-700 px-1 text-base">
+          {off}%
+        </span>
+      </div>
+      <span className="w-fit rounded-md px-1 text-base line-through">
+        &#x20B9;{mrp}
+      </span>
+    </div>
+  ) : (
+    <span className={clsx("inline")}>
+      &#x20B9;
+      {amount}
+    </span>
+  );
 
 export default Price;
